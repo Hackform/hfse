cde4198416fec83835409c6d67fef6aa6a62a3b7
Kevin Wang <wangkevin448@gmail.com>
2016-12-26 23:14:05 -0800

    connect to redis cache

--- service/himeji/cache/cache.go
+++ service/himeji/cache/cache.go
@@ -2,10 +2,55 @@ package cache
 
 import (
 	"github.com/Hackform/hfse/service"
+	"gopkg.in/redis.v5"
 )
 
 type (
+	CacheInfo struct {
+		url, pass string
+		name      int
+	}
+
 	Cache struct {
 		service.ServiceBase
+		CacheInfo
+		database *redis.Client
 	}
 )
+
+func New(url string, name int, pass string) *Cache {
+	return &Cache{
+		CacheInfo: CacheInfo{
+			url:  url,
+			name: name,
+			pass: pass,
+		},
+		database: redis.NewClient(&redis.Options{
+			Addr:     url,
+			Password: pass,
+			DB:       name,
+		}),
+	}
+}
+
+func (c *Cache) Start() {
+	done := c.Connect()
+	<-done
+}
+
+func (c *Cache) Shutdown() {
+	c.Close()
+}
+
+func (c *Cache) Connect() <-chan bool {
+	done := make(chan bool)
+	go func() {
+		_, err := c.database.Ping().Result()
+		done <- err == nil
+	}()
+	return done
+}
+
+func (c *Cache) Close() {
+	c.database.Close()
+}
